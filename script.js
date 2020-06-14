99d445bc948c24fde71802a10cc5f33d81f08175
lafry5 <lafry5@yahoo.com>
2020-06-14 08:26:37 -0700

    tested code and added start of asking user if they want certain characters

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// variables are...
-// userResponse ok or cancel
-// specChars lists the special Characters that can be used in the password
-// passwordlength is the length of the password (should be between 8 and 128)
-passwordlength = 0;
 
-// var specChars = [" ", "!", """, "#", "$", "%", "&", "'", "(", "(", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "'", "{", "|", "}", "~"]
-//console.log(specChars[1], specChars[8]); //Try to print out the Special Characters to see if the string array is working
+// userResponse: ok or cancel
+// specChars: lists the special Characters that can be used in the password
+// lowercase: lists the lower case letters
+// numbers: lists the numbers 8 - 128
+// uppercase: lists the upper case letters
+// passwordlength: the length of the password (should be between 8 and 128)
+
+passwordlength = 0;
+// var specChars = [" ", "!",""", "#", "$", "%", "&", "'", "(", "(", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "'", "{", "|", "}", "~"]
+var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
 // Collect the user's response if they want to generate a password
 var userResponse = confirm('Do you want to generate a password');
@@ -21,7 +25,7 @@ if (userResponse === true) {
       console.log(passwordlength);
 
   //Check password length and execute code if the password lengths falls within the bounds
-  for (var i = 0; i < 3; i++) {
+  for (var i = 0; i < 3; i++) { // Allow for several attempts at entering the password length
       if (passwordlength < 8 || passwordlength > 128) { //Is the password outside of the password bounds?
          var passwordlenth = prompt('Enter a number between 8 and 128!');
         console.log(passwordlength);
@@ -35,19 +39,21 @@ if (userResponse === true) {
           i = 2;
             } //end of else statement that checks password length inside the bounds    
 
-
-        } //end of if statement to check bounds of password length
+          } //end of if statement to check bounds of password length
   } //end of for loop, i loops 3 times
 
-
-
+  if (passwordlength >=8 || passwordlength <=128) {
+    alert('We will execute code here to ask for characters')
+  } else {
+    alert('You exceeded the number of attempts to enter a number between 8 and 128')
+  }
 
 
 // User chose not to generate a password. The end!                  Why is this still appearing if the pw is generated?
-  else (userResponse === false)
-      alert('Have a great day!'); { 
+// else (userResponse === false); {
+// alert('Have a great day!'); 
 //  console.log('You chose not to generate a password. Have a great day!');
-} //end of if statement that checks if user wants to generate a password
+//} //end of if statement that checks if user wants to generate a password
 
 
 
